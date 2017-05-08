5e7a8fe6139efbed300c183c576de430283cdf47
Valeria Benetti <vbenetti46@gmail.com>
2017-05-07 20:16:43 -0400

    Fixed some of the linter errors

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,35 +1,36 @@
-#pick a random number between 1-100
+# pick a random number between 1-100
 number = rand(1...101)
 num_guesses = 0
+guess = []
 
-#prompt user for guess
+# prompt user for guess
 loop do
-    print "Pick a number between 1-100: "
-    guess = gets.chomp.to_i
-    num_guesses += 1
+  print "Pick a number between 1-100: "
+  guess << gets.chomp.to_i
+  num_guesses += 1
 
-#If guess is to high
-    unless guess == number
-    message = if guess > number
+  # If guess is to high
+  unless guess.include? number
+  message =
+  if guess > number
     puts "Your guess is too high."
-
-else
-#If guess is to low
+  else
+    # If guess is to low
     puts "Your guess is too low."
 end
 
-#If guess is correct
-puts message
-else
+  # If guess is correct
+  puts message
+  else
     puts "You guessed right!"
     puts "It took you #{num_guesses} tries to guess my number!"
     break
 end
 
-#if you guess 5 times, you lose the game
-if num_guesses == 5
+  # if you guess 5 times, you lose the game
+  if num_guesses == 5
   puts "You lose the game!"
-break
+  break
 
-end
+  end
 end
